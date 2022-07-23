31a4b08150d5e41a0aa6a3655d6d9e5344e3c1e0
devnoman01 <devnoman01@gmail.com>
2022-07-23 17:01:15 +0600

    dymanic data load in component complete

--- src/App.js
+++ src/App.js
@@ -5,14 +5,33 @@ import "./App.css";
 import DataRow from "./Components/DataRow/DataRow";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemPerPage, setItemPerPage] = useState(3);
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.results);
+      });
+  }, []);
+
+  // get current posts
+  // const indexOfLastItem = currentPage * itemPerPage;
+  // const indexOfFirstItem = indexOfLastItem - itemPerPage;
+  // const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
   return (
     <>
       <div className="w-full bg-[#F4F7FC]">
         <div className="container border py-8 mx-auto">
           <div className="bg-gray-100 px-3 py-4 border">
-            {/* user row */}
-            <DataRow />
-            <DataRow />
+            {/* data row */}
+            {data.map((item, index) => (
+              <DataRow key={index} item={item} />
+            ))}
           </div>
         </div>
       </div>
--- src/Components/DataRow/DataRow.js
+++ src/Components/DataRow/DataRow.js
@@ -1,25 +1,43 @@
 import React, { useState } from "react";
 
-const DataRow = () => {
+const DataRow = ({ item }) => {
+  const {
+    name,
+    gender,
+    height,
+    mass,
+    hair_color,
+    skin_color,
+    eye_color,
+    birth_year,
+    homeworld,
+    films,
+    species,
+    vehicles,
+    starships,
+    created,
+    edited,
+    url,
+  } = item;
   const [detailView, setDetailView] = useState(false);
 
   return (
     <div className="bg-white p-5 md:p-8 lg:p-10 mb-5 rounded-3xl border shadow-sm">
       <div className={`grid grid-cols-1 lg:grid-cols-5 gap-x-8 gap-y-4`}>
         <div className="w-full flex items-center mb-3 lg:mb-0">
-          <h1>Luke Skywalker</h1>
+          <h1>{name}</h1>
         </div>
         <div className="w-full h-full">
           <h5 className="font-semibold mb-1">GENDER</h5>
-          <p>Male</p>
+          <p>{gender}</p>
         </div>
         <div className="w-full h-full">
           <h5 className="font-semibold mb-1">Height</h5>
-          <p>172</p>
+          <p>{height}</p>
         </div>
         <div className="w-full h-full">
           <h5 className="font-semibold mb-1">MASS</h5>
-          <p>77</p>
+          <p>{mass}</p>
         </div>
         <div className="w-full h-full flex justify-center mt-3 md:mt-0 md:justify-end items-center">
           {detailView ? (
@@ -51,51 +69,51 @@ const DataRow = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-5">
           <div className="w-full">
             <h5 className="font-semibold mb-2">Hair Color</h5>
-            <p>blond</p>
+            <p>{hair_color}</p>
           </div>
           <div className="w-full">
             <h5 className="font-semibold mb-2">Skin Color</h5>
-            <p>Gold</p>
+            <p>{skin_color}</p>
           </div>
           <div className="w-full">
             <h5 className="font-semibold mb-2">Eye Color</h5>
-            <p>blue</p>
+            <p>{eye_color}</p>
           </div>
           <div className="w-full">
             <h5 className="font-semibold mb-2">Birth Year</h5>
-            <p>77</p>
+            <p>{birth_year}</p>
           </div>
           <div className="w-full">
             <h5 className="font-semibold mb-2">Homeworld</h5>
-            <p>https://swapi.dev/api/planets/1/</p>
+            <p>{homeworld}</p>
           </div>
           <div className="w-full">
             <h5 className="font-semibold mb-2">Films</h5>
-            <p>4</p>
+            <p>{films.length}</p>
           </div>
           <div className="w-full">
             <h5 className="font-semibold mb-2">Species</h5>
-            <p>0</p>
+            <p>{species.length}</p>
           </div>
           <div className="w-full">
             <h5 className="font-semibold mb-2">Vehicles</h5>
-            <p>2</p>
+            <p>{vehicles.length}</p>
           </div>
           <div className="w-full">
             <h5 className="font-semibold mb-2">Starships</h5>
-            <p>2</p>
+            <p>{starships.length}</p>
           </div>
           <div className="w-full">
             <h5 className="font-semibold mb-2">Created</h5>
-            <p>2014-12-09T13:50:51.644000Z</p>
+            <p>{created}</p>
           </div>
           <div className="w-full">
             <h5 className="font-semibold mb-2">Edited</h5>
-            <p>2014-12-20T21:17:56.891000Z</p>
+            <p>{edited}</p>
           </div>
           <div className="w-full">
             <h5 className="font-semibold mb-2">URL</h5>
-            <p>https://swapi.dev/api/people/1/</p>
+            <p>{url}</p>
           </div>
         </div>
       </div>
